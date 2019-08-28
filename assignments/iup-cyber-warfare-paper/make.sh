9f2229afcf1c9c85b67f5fed0049e7faec8582a2
Henry Post <HenryFBP@gmail.com>
2019-08-27 20:34:03 -0500

    add make script

--- assignments/iup-cyber-warfare-paper/make.sh
+++ assignments/iup-cyber-warfare-paper/make.sh
@@ -1,1 +1,7 @@
-pandoc -s -o out.pdf paper.md
+#!/usr/bin/env bash
+
+inotifywait -r -m -e modify paper.md |
+  while read path _ file; do
+    echo "Building pdf..."
+    pandoc -s -o hpost-aug-28-paper.out.pdf paper.md
+  done
