9b95ada5930ead9f8bd57d4afb1f0eff8add1601
Henry Post <HenryFBP@gmail.com>
2019-10-08 21:11:57 -0500

    download files

---
+++ assignments/software-code-threat-analysis-group/get-files.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+mkdir -p './out/'
+
+if [[ ! -f './out/moodle/moodle-source.zip' ]]; then
+    mkdir -p 'out/moodle/'
+    curl https://ayera.dl.sourceforge.net/project/moodle/Moodle/stable37/moodle-latest-37.zip > ./out/moodle/moodle-source.zip
+fi
+
+if [[ ! -f './out/osu/osu-source.zip' ]]; then
+    mkdir -p 'out/osu/'
+    curl https://ayera.dl.sourceforge.net/project/osu.mirror/2019.930.0/2019.930.0.zip > ./out/osu/osu-source.zip
+fi
+
+if [[ ! -f './out/qbittorrent/qbittorrent-source.zip' ]]; then
+    mkdir -p 'out/qbittorrent/'
+    curl https://managedway.dl.sourceforge.net/project/qbittorrent/qbittorrent/qbittorrent-4.1.5/qbittorrent-4.1.5.tar.xz > ./out/qbittorrent/qbittorrent-source.zip
+fi
+
