707e26501d37e0e7ba0df8a97d12e28a5384286a
Henry Post <HenryFBP@gmail.com>
2019-10-09 13:47:24 -0500

    add cppcheck

--- assignments/software-code-threat-analysis-group/analyze-flaws.sh
+++ assignments/software-code-threat-analysis-group/analyze-flaws.sh
@@ -3,6 +3,20 @@
 mkdir -p analysis/flawfinder
 mkdir -p analysis/cppcheck
 
-flawfinder out/qbittorrent/ > analysis/flawfinder/qbittorrent-report.txt
-flawfinder out/dosbox/ > analysis/flawfinder/dosbox-report.txt
-flawfinder out/gnucash/ > analysis/flawfinder/gnucash-report.txt
+echo "flawfinder for qbittorrent"
+flawfinder out/qbittorrent/ > analysis/flawfinder/qbittorrent-report.txt 2>&1
+
+echo "flawfinder for dosbox"
+flawfinder out/dosbox/ > analysis/flawfinder/dosbox-report.txt 2>&1
+
+echo "flawfinder for gnucash"
+flawfinder out/gnucash/ > analysis/flawfinder/gnucash-report.txt 2>&1
+
+echo "cppcheck for qbittorrent"
+cppcheck -q --force out/qbittorrent/ > analysis/cppcheck/qbittorrent-report.txt 2>&1
+
+echo "cppcheck for dosbox"
+cppcheck -q --force out/dosbox/ > analysis/cppcheck/dosbox-report.txt 2>&1
+
+echo "cppcheck for gnucash"
+cppcheck -q --force out/gnucash/ > analysis/cppcheck/gnucash-report.txt 2>&1
