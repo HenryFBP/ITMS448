f1902faca0f61e271952b281a78badffb1184b87
Henry Post <HenryFBP@gmail.com>
2019-10-08 22:21:40 -0500

    unzip dl'd source code

--- assignments/software-code-threat-analysis-group/get-files.sh
+++ assignments/software-code-threat-analysis-group/get-files.sh
@@ -4,16 +4,25 @@ mkdir -p './out/'
 
 if [[ ! -f './out/moodle/moodle-source.zip' ]]; then
     mkdir -p 'out/moodle/'
-    curl https://ayera.dl.sourceforge.net/project/moodle/Moodle/stable37/moodle-latest-37.zip > ./out/moodle/moodle-source.zip
+    pushd 'out/moodle'
+    curl https://ayera.dl.sourceforge.net/project/moodle/Moodle/stable37/moodle-latest-37.zip > ./moodle-source.zip
+    unzip ./moodle-source.zip
+    popd
 fi
 
 if [[ ! -f './out/osu/osu-source.zip' ]]; then
     mkdir -p 'out/osu/'
-    curl https://ayera.dl.sourceforge.net/project/osu.mirror/2019.930.0/2019.930.0.zip > ./out/osu/osu-source.zip
+    pushd 'out/osu'
+    curl https://ayera.dl.sourceforge.net/project/osu.mirror/2019.930.0/2019.930.0.zip > ./osu-source.zip
+    unzip ./osu-source.zip
+    popd
 fi
 
 if [[ ! -f './out/qbittorrent/qbittorrent-source.tar.xz' ]]; then
     mkdir -p 'out/qbittorrent/'
-    curl https://managedway.dl.sourceforge.net/project/qbittorrent/qbittorrent/qbittorrent-4.1.5/qbittorrent-4.1.5.tar.xz > ./out/qbittorrent/qbittorrent-source.tar.xz
+    pushd 'out/qbittorrent/'
+    curl https://managedway.dl.sourceforge.net/project/qbittorrent/qbittorrent/qbittorrent-4.1.5/qbittorrent-4.1.5.tar.xz > ./qbittorrent-source.tar.xz
+    tar -xf ./qbittorrent-source.tar.xz
+    popd
 fi
 
